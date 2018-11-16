0809dd2cd67f905bc1d74d60c258101749e9a95f
Younguk Kim <yukim@chic.re.kr>
2018-11-16 22:28:57 +0900

    [openvr] Fix missing virtual keyword

--- openvr/include/openvr_plugin.hpp
+++ openvr/include/openvr_plugin.hpp
@@ -68,8 +68,8 @@ public:
 
     virtual vr::IVRSystem* get_vr_system() const;
 
-    NodePath load_model(const std::string& model_name) const;
-    NodePath load_model(vr::TrackedDeviceIndex_t unTrackedDeviceIndex) const;
+    virtual NodePath load_model(const std::string& model_name) const;
+    virtual NodePath load_model(vr::TrackedDeviceIndex_t unTrackedDeviceIndex) const;
 
     virtual NodePath setup_device_node(vr::TrackedDeviceIndex_t unTrackedDeviceIndex);
     virtual NodePath setup_render_model(vr::TrackedDeviceIndex_t unTrackedDeviceIndex);
